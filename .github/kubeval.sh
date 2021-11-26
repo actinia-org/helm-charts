8aa88f34608ea4b9cacc57956d2a4b4f047e5496
Carmen <carmen.tawalika@gmail.com>
2021-11-26 11:56:24 +0100

    test gha

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
+# CHART_DIRS="charts/actinia  charts/openeo-grassgis-driver  charts/openeo-web-editor"
 KUBEVAL_VERSION="0.14.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
@@ -12,4 +13,5 @@ tar -xf /tmp/kubeval.tar.gz kubeval
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
   helm template "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
+  echo "Exit code for ${CHART_DIR} is $?"
 done
