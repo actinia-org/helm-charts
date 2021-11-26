02c2f0cc1bd979c462f16cc68d3ea26e75d8d678
Carmen <carmen.tawalika@gmail.com>
2021-11-26 12:14:54 +0100

    test gha

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/main -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-# CHART_DIRS="charts/actinia  charts/openeo-grassgis-driver  charts/openeo-web-editor"
 KUBEVAL_VERSION="0.14.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
@@ -10,11 +9,9 @@ SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-s
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
 
-echo $CHART_DIRS
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
-  echo $CHART_DIR
+  echo "Validating $CHART_DIR"
   (cd $CHART_DIR && helm dep up)
   helm template "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
-  echo "Exit code for ${CHART_DIR} is $?"
 done
