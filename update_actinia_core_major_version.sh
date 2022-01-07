df205b8443ffefe81213a5b9b9f28516da964719
anikaweinmann <aweinmann@mundialis.de>
2022-01-07 13:57:21 +0100

    CT review

--- update_actinia_core_major_version.sh
+++ update_actinia_core_major_version.sh
@@ -7,7 +7,7 @@
 #
 # PURPOSE:      This script updates the major actinia-core version in url
 #
-# COPYRIGHT:    (C) 2021 by mundialis GmbH & Co. KG
+# COPYRIGHT:    (C) 2022 by mundialis GmbH & Co. KG
 #
 #               This program is free software under the GNU General Public
 #               License (>=v3). Read the file COPYING that comes with GRASS
