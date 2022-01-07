d91c96e6316cd374ff409c56ef6872c57be8b7c0
anikaweinmann <aweinmann@mundialis.de>
2022-01-07 11:32:14 +0100

    v3

---
+++ update_actinia_core_major_version.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+############################################################################
+#
+# MODULE:       update_actinia_core_major_version.sh
+# AUTHOR(S):    Anika Weinmann
+#
+# PURPOSE:      This script updates the major actinia-core version in url
+#
+# COPYRIGHT:    (C) 2021 by mundialis GmbH & Co. KG
+#
+#               This program is free software under the GNU General Public
+#               License (>=v3). Read the file COPYING that comes with GRASS
+#               for details.
+#
+#############################################################################
+
+# execution for example with.
+#     bash update_actinia_core_major_version.sh v2 v3
+
+OLD_VERSION=$1
+NEW_VERSION=$2
+
+sed -i "s+/api/${OLD_VERSION}/+/api/${NEW_VERSION}/+g" charts/actinia/templates/NOTES.txt
+sed -i "s+/api/${OLD_VERSION}/+/api/${NEW_VERSION}/+g" charts/actinia/templates/statefulset.yaml
